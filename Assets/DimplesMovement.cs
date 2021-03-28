4fd6b40d457580aa46e3deb8ff92c9dd48d40ba3
Natasha Bullimore <natasha.bullimore@live.co.uk>
2021-03-28 11:59:06 +0100

    more animations

---
+++ Assets/DimplesMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DimplesMovement : MonoBehaviour
+{
+    public Animator animator;
+    public CharacterController2D controller;
+    Animator animator = playerTransform.gameObject.GetComponent<Animator>();
+    animator.runtimeAnimatorController = Resources.Load("path_to_your_controller") as RuntimeAnimatorController;
+        
+    // Start is called before the first frame update
+    //void Start()
+    {
+        //
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
