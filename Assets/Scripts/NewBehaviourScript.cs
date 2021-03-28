ef5fc3f16de92012157b579264b366e8f1f59ded
Natasha Bullimore <natasha.bullimore@live.co.uk>
2021-03-28 12:09:11 +0100

    test

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public bool Attack = false;
     // Start is called before the first frame update
     void Start()
     {
