82f126ef8eef169832aff7a178f3ecd7bdb106de
Natasha Bullimore <natasha.bullimore@live.co.uk>
2021-03-29 11:40:28 +0100

    main menu

---
+++ Assets/Scripts/LinkedCode.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class linkedCode : MonoBehaviour
+{
+    public static int linkedNumber;
+    public int internalNumber;
+
+    // Update is called once per frame
+    void Update()
+    {
+        internalNumber = linkedNumber;
+        
+    }
+}
