9457325e06011d39f15a4c0925eb7d03a02d29fb
Nathan Coburn <nathan.coburn12@outlook.com>
2021-03-29 13:10:39 +0100

    Collision Detection

---
+++ Assets/LeftHandActions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftHandActions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col){
+        switch(col.gameObject.tag){
+            case "Opponent":
+                print("You have hit the opponent");
+                break;
+        }
+    }
+}
